8673018afd3cd9d6bdb78a0bf5f1123bd5a9030e
EC2 Default User <ec2-user@ip-172-31-40-6.ec2.internal>
2022-04-18 11:16:45 +0000

    [Finish]アプリケーションを完成させよう２

---
+++ config/routes.rb
@@ -0,0 +1,10 @@
+Rails.application.routes.draw do
+  devise_for :users
+  root to: "homes#top"
+  get 'home/about',  to: 'homes#about', as: :about
+  resources :users, only:[:show, :edit, :update]
+  get 'users', to: 'users#index', as: :users
+  resources :books
+
+  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+end
